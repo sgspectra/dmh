8b1935bb27223641ce2313493cd656cfa0758193
Andrew Storch <astorch13@gmail.com>
2020-08-20 16:41:41 -0400

    got a modal to appear in the characters component, it will be used to add a character

--- src/app/character/character.component.ts
+++ src/app/character/character.component.ts
@@ -4,6 +4,7 @@ import { AuthenticationService } from '../authentication.service';
 import {Character} from '../character';
 import { Observable } from 'rxjs';
 import {CharacterService} from '../character.service';
+import { NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-character',
@@ -11,9 +12,11 @@ import {CharacterService} from '../character.service';
   styleUrls: ['./character.component.scss']
 })
 export class CharacterComponent implements OnInit {
+  closeResult: string;
   characters: Character[];
   userId: number;
-  constructor(private characterService: CharacterService, private authenticationService: AuthenticationService) { }
+  constructor(private characterService: CharacterService, private authenticationService: AuthenticationService,
+              private modalService: NgbModal) { }
 
   ngOnInit(): void {
     if (localStorage.getItem('isLoggedIn') === 'true'){
@@ -28,5 +31,20 @@ export class CharacterComponent implements OnInit {
       this.characters = characters;
     });
   }
-
+  open(content): void {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
 }
